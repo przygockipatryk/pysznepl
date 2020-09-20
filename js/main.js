5e07dab77bf6e4af6b41b018fe161b3a4fa035b3
przygockipatryk <przygocki.patryk11@gmail.com>
2020-09-20 10:44:13 +0200

    cwiczenia DOM

--- js/main.js
+++ js/main.js
@@ -3,22 +3,38 @@
 // const input = document.getElementsByClassName('margin-bottom');
 // console.log("input po klasie", input)
 const idBtn = document.getElementById('id-btn');
-console.log("button po id", idBtn)
+
 
 
 
 idBtn.addEventListener("click", takeOrder)
 
 function takeOrder() {
+    sprawdzanieSosow("sosy")
     const pomidorowy = document.getElementById('pomidorowy')
+    const czosnkowy = document.getElementById('czosnkowy')
+    const ostryPomidorowy = document.getElementById('ostryPomidorowy')
     console.log(pomidorowy.checked)
 }
+
+function sprawdzanieSosow(nazwaKlasy) {
+    const pobraneWszytkieSosyZinputu = document.getElementsByClassName(nazwaKlasy)
+    for (let i = 0; i < pobraneWszytkieSosyZinputu.length; i++) {
+        // console.log(pobraneWszytkieSosyZinputu[i])
+        if (pobraneWszytkieSosyZinputu[i].checked === true) {
+            console.log(pobraneWszytkieSosyZinputu[i].attributes)
+        }
+    }
+}
+
 
 // function dodawanie(a, b) {
 //     let wynik = a + b
 //     console.log(wynik)
 // }
 // dodawanie(5, 15)
-// dodawanie(5, 198)
+
+// dodawanie(2, 15)
+// dodawanie(7, 1885)
 // dodawanie(5, 88)
-// dodawanie(5, 15)
+// dodawanie(7, 7)
